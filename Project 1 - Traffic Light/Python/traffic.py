37599554ead490946be4653412c2ba6d67208abe
Les Pounder <lespounder@gmail.com>
2014-05-07 19:46:11 +0100

    updated traffic.py to use to include a loop which controls the light sequence

--- Project 1 - Traffic Light/Python/traffic.py
+++ Project 1 - Traffic Light/Python/traffic.py
@@ -8,16 +8,24 @@ delay = 10
 sequence = 2
 
 #Create the sequence
-
-#Green on for 10 seconds
-pibrella.light.green.on()
-time.sleep(delay)
-pibrella.light.green.off()
-#Amber for 2 seconds
-pibrella.light.amber.on()
-time.sleep(sequence)
-pibrella.light.amber.off()
-#Red for 10 seconds.
-pibrella.light.red.on()
-time.sleep(delay)
-pibrella.light.red.off()
+while True:
+    #Green on for 10 seconds
+    print("GREEN")
+    pibrella.light.green.on()
+    time.sleep(delay)
+    pibrella.light.green.off()
+    #Amber for 2 seconds
+    print("AMBER")
+    pibrella.light.amber.on()
+    time.sleep(sequence)
+    pibrella.light.amber.off()
+    #Red for 10 seconds.
+    print("RED")
+    pibrella.light.red.on()
+    time.sleep(delay)
+    #Don't turn off the red light until the end of the amber sequence.
+    pibrella.light.red.off()
+    print("AMBER")
+    pibrella.light.amber.on()
+    time.sleep(sequence)
+    pibrella.light.amber.off()    
