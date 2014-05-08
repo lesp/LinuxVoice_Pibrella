7c3643e8891073bd81a0236f52c577028189909d
Les Pounder <lespounder@gmail.com>
2014-05-08 10:59:54 +0100

    updated the code to flash LED to indicate the guessed number

--- Project 2 - Dice Game/Python/dicegame.py
+++ Project 2 - Dice Game/Python/dicegame.py
@@ -6,7 +6,10 @@ def dice():
     guess = random.randint(1,6)
     print("I'm going to think of a number between 1 and 6 and I will flash the lights on your Pibrella to indicate the number I chose")
     print("The number is "+(guess))
-(guess)
+    for i in range(guess):
+        pibrella.light.on()
+        time.sleep(0.5)
+        pibrella.light.off()
 
 def standby():
     for i in range(6):
